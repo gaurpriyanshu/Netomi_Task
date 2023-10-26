03e33ae01ee09833e9aef2e2b22e01e59294d4fc
gaurpriyanshu <priyanshu.22.gaur@gmail.com>
2023-10-26 19:41:05 +0530

    Commit 3

--- src/main/java/com/example/Netmoi/Netomi.java
+++ src/main/java/com/example/Netmoi/Netomi.java
@@ -1,9 +1,12 @@
 package com.example.Netmoi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class Netomi {
 
     public static void main(String[] args) {
